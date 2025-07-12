27e4a3b78aa1dc60b7a5b980d4b40d3f98fbf5ca
anuragsharma2401 <anuragsharma2407r@gmail.com>
2025-07-12 12:52:45 +0530

    Completed

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("delete_transaction/<id>",views.delete_transaction,name="delete_transaction")
 ]
